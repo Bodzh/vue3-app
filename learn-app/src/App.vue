75f6f74b4ef30139c3d85e31468abc870800fb04
Bogdan Novak <bogdan.novak@altium.com>
2023-04-05 17:08:18 +0200

    added router

--- learn-app/src/App.vue
+++ learn-app/src/App.vue
@@ -1,18 +1,8 @@
 <script setup lang="ts">
-import TimeLine from './components/TimeLine.vue'
 </script>
 
 <template>
-  <div class="section">
-    <div class="container">
-      <Suspense>
-        <TimeLine />
-        <template #fallback>
-          <progress class="progress is-primary is-small" />
-        </template>
-      </Suspense>
-    </div>
-  </div>
+<RouterView />
 </template>
 
 <style>
